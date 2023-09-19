57989c2152869cbfb87883c34b3a54f8450d9eb4
jfontvit <jordifv90@gmail.com>
2023-09-19 12:52:29 +0200

    Add getBookByID method on BookService

--- src/main/java/com/jordi/booknook/services/BookService.java
+++ src/main/java/com/jordi/booknook/services/BookService.java
@@ -5,6 +5,7 @@ import com.jordi.booknook.models.BookRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
@@ -18,7 +19,7 @@ public class BookService {
         return bookRepository.findAll();
     }
 
-
-
-
+    public Optional<BookEntity> getBookById(Long book_id) {
+        return bookRepository.findById(book_id);
+    }
 }
