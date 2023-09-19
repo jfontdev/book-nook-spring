26c15e1848b613b7eaad2c320223c58588fd4a35
jfontvit <jordifv90@gmail.com>
2023-09-19 14:25:13 +0200

    Add search endpoint, search books with title or description

--- src/main/java/com/jordi/booknook/controllers/BookController.java
+++ src/main/java/com/jordi/booknook/controllers/BookController.java
@@ -2,12 +2,10 @@ package com.jordi.booknook.controllers;
 
 
 import com.jordi.booknook.models.BookEntity;
+import com.jordi.booknook.models.UniversalSearch;
 import com.jordi.booknook.services.BookService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,5 +31,8 @@ public class BookController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-
+    @PostMapping("/search")
+    public List<BookEntity> searchBooks(@RequestBody UniversalSearch search){
+        return bookService.search(search);
+    }
 }
