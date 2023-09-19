a1d6f388f35912eade0c85a57611d1ee79e419c3
jfontvit <jordifv90@gmail.com>
2023-09-19 09:30:25 +0200

    Add CategoryRepository

---
+++ src/main/java/com/jordi/booknook/models/CategoryRepository.java
@@ -0,0 +1,5 @@
+package com.jordi.booknook.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> { }
