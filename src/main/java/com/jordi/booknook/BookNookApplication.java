a358e40e7daa6a2b0408ba788321a787b7289b2f
jfontvit <jordifv90@gmail.com>
2023-09-19 07:51:53 +0200

    Initial Commit

---
+++ src/main/java/com/jordi/booknook/BookNookApplication.java
@@ -0,0 +1,13 @@
+package com.jordi.booknook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNookApplication.class, args);
+	}
+
+}
