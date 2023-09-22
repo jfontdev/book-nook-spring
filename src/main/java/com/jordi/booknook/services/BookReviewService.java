f86461f3a27e09c9b1009d085d4bf0bf4ccddb48
jfontvit <jordifv90@gmail.com>
2023-09-22 15:06:22 +0200

    Add getReviewsByUser method that get the current auth user, check the db for that user and get the reviews for that user

--- src/main/java/com/jordi/booknook/services/BookReviewService.java
+++ src/main/java/com/jordi/booknook/services/BookReviewService.java
@@ -2,9 +2,15 @@ package com.jordi.booknook.services;
 
 import com.jordi.booknook.models.BookEntity;
 import com.jordi.booknook.models.BookReviewEntity;
+import com.jordi.booknook.models.UserEntity;
 import com.jordi.booknook.payload.response.ReviewsByBookResponse;
+import com.jordi.booknook.payload.response.ReviewsByUserResponse;
 import com.jordi.booknook.repositories.BookRepository;
 import com.jordi.booknook.repositories.BookReviewRepository;
+import com.jordi.booknook.repositories.UserRepository;
+import com.jordi.booknook.security.UserDetailsImplementation;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -15,10 +21,12 @@ import java.util.Optional;
 public class BookReviewService {
     private final BookReviewRepository bookReviewRepository;
     private final BookRepository bookRepository;
+    private final UserRepository userRepository;
 
-    public BookReviewService(BookReviewRepository bookReviewRepository, BookRepository bookRepository) {
+    public BookReviewService(BookReviewRepository bookReviewRepository, BookRepository bookRepository, UserRepository userRepository) {
         this.bookReviewRepository = bookReviewRepository;
         this.bookRepository = bookRepository;
+        this.userRepository = userRepository;
     }
 
     public ReviewsByBookResponse getReviewsByBook (Long book_id){
@@ -32,5 +40,17 @@ public class BookReviewService {
         }
     }
 
+    public ReviewsByUserResponse getReviewsByUser (){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImplementation userDetails = (UserDetailsImplementation) auth.getPrincipal();
 
+        Optional<UserEntity> authenticatedUser = userRepository.findByUsername(userDetails.getUsername());
+
+        if (authenticatedUser.isPresent()){
+            List<BookReviewEntity> reviews = bookReviewRepository.findBookReviewEntitiesByUser(authenticatedUser.get());
+            return new ReviewsByUserResponse(reviews);
+        }else {
+            return new ReviewsByUserResponse( Collections.emptyList());
+        }
+    }
 }
