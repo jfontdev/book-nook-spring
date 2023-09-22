c14c726c80a4817bb0d2751d02db6ebc977a5d46
jfontvit <jordifv90@gmail.com>
2023-09-22 13:14:33 +0200

    Add BookReviewController with getReviewsByBook endpoint

---
+++ src/main/java/com/jordi/booknook/controllers/BookReviewController.java
@@ -0,0 +1,27 @@
+package com.jordi.booknook.controllers;
+
+import com.jordi.booknook.payload.response.ReviewsByBookResponse;
+import com.jordi.booknook.services.BookReviewService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("api/v1/reviews")
+public class BookReviewController {
+    private final BookReviewService bookReviewService;
+
+    public BookReviewController(BookReviewService bookReviewService) {
+        this.bookReviewService = bookReviewService;
+    }
+
+    @GetMapping("/{book_id}/get")
+    public ResponseEntity<ReviewsByBookResponse> getReviewsByBook (@PathVariable Long book_id){
+        ReviewsByBookResponse response = bookReviewService.getReviewsByBook(book_id);
+
+        return ResponseEntity.ok(response);
+    }
+}
