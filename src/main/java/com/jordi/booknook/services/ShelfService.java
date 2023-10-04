baa44a63f9f9bbf54eab5b9b909f0ecfff21bc31
jfontvit <jordifv90@gmail.com>
2023-10-04 13:29:23 +0200

    Add addBookToShelf service method

--- src/main/java/com/jordi/booknook/services/ShelfService.java
+++ src/main/java/com/jordi/booknook/services/ShelfService.java
@@ -1,15 +1,20 @@
 package com.jordi.booknook.services;
 
+import com.jordi.booknook.models.BookEntity;
 import com.jordi.booknook.models.ShelfEntity;
 import com.jordi.booknook.models.UserEntity;
+import com.jordi.booknook.payload.request.AddBookToShelfRequest;
 import com.jordi.booknook.payload.request.NewShelfRequest;
 import com.jordi.booknook.payload.request.UpdateShelfRequest;
+import com.jordi.booknook.payload.response.AddBookToShelfResponse;
 import com.jordi.booknook.payload.response.NewShelfResponse;
 import com.jordi.booknook.payload.response.UpdateShelfResponse;
+import com.jordi.booknook.repositories.BookRepository;
 import com.jordi.booknook.repositories.ShelfRepository;
 import com.jordi.booknook.repositories.UserRepository;
 import com.jordi.booknook.security.UserDetailsImplementation;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -20,10 +25,12 @@ import java.util.Optional;
 public class ShelfService {
     private final ShelfRepository shelfRepository;
     private final UserRepository userRepository;
+    private final BookRepository bookRepository;
 
-    public ShelfService(ShelfRepository shelfRepository, UserRepository userRepository) {
+    public ShelfService(ShelfRepository shelfRepository, UserRepository userRepository, BookRepository bookRepository) {
         this.shelfRepository = shelfRepository;
         this.userRepository = userRepository;
+        this.bookRepository = bookRepository;
     }
 
     public NewShelfResponse addNewShelf(NewShelfRequest request) {
@@ -69,5 +76,35 @@ public class ShelfService {
         } else {
             throw new EntityNotFoundException("Shelf Not found.");
         }
+    }
+
+    public AddBookToShelfResponse addBookToShelf(AddBookToShelfRequest request){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImplementation userDetails = (UserDetailsImplementation) auth.getPrincipal();
+
+        Optional<UserEntity> authenticatedUser = userRepository.findByUsername(userDetails.getUsername());
+        Optional<BookEntity> bookToAdd = bookRepository.findById(request.book_id());
+        Optional<ShelfEntity> shelf = shelfRepository.findById(request.shelf_id());
+
+        if (bookToAdd.isEmpty()){
+            throw new EntityNotFoundException("Book Not Found.");
+        }
+
+        if (shelf.isEmpty()){
+            throw new EntityNotFoundException("Shelf Not Found.");
+        }
+
+        // Todo: Fix Servlet AuthException handling (AuthEntryPointJwt).
+        if ((authenticatedUser.isPresent() && authenticatedUser.get() != shelf.get().getUser())){
+            throw new AccessDeniedException("Not allowed to add a book to that shelf.");
+        }
+
+        ShelfEntity updatedShelf = shelf.get();
+        updatedShelf.getBooks().add(bookToAdd.get());
+        BookEntity lastBook = updatedShelf.getBooks().stream().reduce((one, two) -> two).get();
+
+        shelfRepository.save(updatedShelf);
+
+        return new AddBookToShelfResponse(lastBook,updatedShelf);
     }
 }
