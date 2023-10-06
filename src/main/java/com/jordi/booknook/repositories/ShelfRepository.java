e46fe96baa35fa0234aacc27d597d195ad01e5e8
jfontvit <jordifv90@gmail.com>
2023-10-06 12:26:48 +0200

    Add findAllUserAndPublicShelfIsFalse query method

--- src/main/java/com/jordi/booknook/repositories/ShelfRepository.java
+++ src/main/java/com/jordi/booknook/repositories/ShelfRepository.java
@@ -5,7 +5,6 @@ import com.jordi.booknook.models.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -15,4 +14,7 @@ public interface ShelfRepository extends JpaRepository<ShelfEntity, Long> {
 
     @Query("SELECT shelve FROM ShelfEntity shelve WHERE shelve.user = :user AND shelve.shelf_id = :shelfId")
     Optional<ShelfEntity> findOneByUserAndShelfId(@Param("user") UserEntity user, @Param("shelfId") Long shelf_id);
+
+    @Query("SELECT shelves from ShelfEntity shelves WHERE shelves.user = :user AND shelves.public_shelf = false")
+    List<ShelfEntity> findAllByUserAndPublicShelfIsFalse(@Param("user") UserEntity user);
 }
