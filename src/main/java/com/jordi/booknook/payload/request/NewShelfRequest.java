548391ef11bdc2dd07dc27ea88f804add57ef2b9
Jordi <jordifv90@gmail.com>
2023-10-17 13:09:00 +0200

    Add validation messages to the NewShelfRequest record.

--- src/main/java/com/jordi/booknook/payload/request/NewShelfRequest.java
+++ src/main/java/com/jordi/booknook/payload/request/NewShelfRequest.java
@@ -4,9 +4,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record NewShelfRequest(
-        @NotBlank String name,
-        @NotBlank String image,
-        @NotBlank String description,
-        @NotNull Boolean public_shelf
+        @NotBlank(message = "Name is required.") String name,
+        @NotBlank(message = "Image URL is required.") String image,
+        @NotBlank(message = "Description is required.") String description,
+        @NotNull(message = "Public shelf is required.") Boolean public_shelf
 ) {
 }
