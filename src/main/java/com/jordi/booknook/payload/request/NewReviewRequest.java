2c7c82bc6224eccb20f041333307c4a739448e4b
Jordi <jordifv90@gmail.com>
2023-10-18 08:12:59 +0200

    Add error messages to the validations in NewReviewRequest

--- src/main/java/com/jordi/booknook/payload/request/NewReviewRequest.java
+++ src/main/java/com/jordi/booknook/payload/request/NewReviewRequest.java
@@ -5,7 +5,12 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record NewReviewRequest(
-        @NotNull Long book_id,
-        @NotNull @Min(1) @Max(5) Integer rating,
-        String review) {
+        @NotNull(message = "Book id is required.")
+        Long book_id,
+        @NotNull(message = "Rating is required.")
+        @Min(value = 1, message = "Minimum value is 1.")
+        @Max(value = 5, message = "Must be less than or equal to 5.")
+        Integer rating,
+        String review
+) {
 }
