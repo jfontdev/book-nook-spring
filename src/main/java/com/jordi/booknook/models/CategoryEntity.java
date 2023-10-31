b1d3d6d0a1a4d7290dbd9c88b79011c552d9d93e
Jordi <jordifv90@gmail.com>
2023-10-31 16:16:48 +0100

    Add generated id type strategy to IDENTITY to ensure that our db uses AUTO_INCREMENT ids

--- src/main/java/com/jordi/booknook/models/CategoryEntity.java
+++ src/main/java/com/jordi/booknook/models/CategoryEntity.java
@@ -11,7 +11,7 @@ import java.util.Set;
 @Entity
 public class CategoryEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long category_id;
 
     String name;
